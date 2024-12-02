9fb3332b9e7e47d89bed2f95982f2cf6125b3b07
Nodenyx <albin@alteborn.se>
2024-12-02 10:10:43 +0100

    Work on dialogue system and initial parts of intro cutscene.

--- Assets/Albin_Testing_Assets/Scripts/DialogueManager.cs
+++ Assets/Albin_Testing_Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;   
 
 public class DialogueManager : MonoBehaviour
 {
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    
+    public Animator animator;
     private Queue<string> sentences; 
     
     // Start is called before the first frame update
@@ -14,7 +19,9 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        animator.SetBool("IsOpen", true);
+        
+        nameText.text = dialogue.name;
         
         sentences.Clear();
 
@@ -36,12 +43,23 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
     }
     
     void EndDialogue()
     {
-        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
     }
     // Update is called once per frame
     
