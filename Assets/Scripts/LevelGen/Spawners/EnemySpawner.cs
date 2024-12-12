dde3a15358aac71f047b11868e8191e0cdc5ff0b
Ghoust <gustavw02@gmail.com>
2024-12-12 16:46:45 +0100

    Fixed doors and pathfinding in TutorialScene

--- Assets/Scripts/LevelGen/Spawners/EnemySpawner.cs
+++ Assets/Scripts/LevelGen/Spawners/EnemySpawner.cs
@@ -7,9 +7,10 @@ public class EnemySpawner : MonoBehaviour
 {
 
     [Header("Enemies")] [Tooltip("Each arena room will contain between x and y amount of enemies")] [SerializeField]
-    private Vector2Int enemyAmountRangeArena1 = new(3, 5);
-    private Vector2Int enemyAmountRangeArena2 = new(5, 7);
-    private Vector2Int enemyAmountRangeArena3 = new(8, 10);
+    private bool doSpawnEnemies;
+    [SerializeField]private Vector2Int enemyAmountRangeArena1 = new(3, 5);
+    [SerializeField]private Vector2Int enemyAmountRangeArena2 = new(5, 7);
+    [SerializeField]private Vector2Int enemyAmountRangeArena3 = new(8, 10);
 
     public GameObject MeleeEnemyPrefab;
     public GameObject RangedEnemyPrefab;
@@ -22,38 +23,69 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemies(LevelMap levelMap)
     {
-        //regenerate room shape lists
-        for (int r = 0; r < levelMap.rooms.Count; r++)
+        if (doSpawnEnemies)
         {
-            Room room = levelMap.rooms[r];
-
-            //place enemies
-            if (room.type == RoomType.Arena1 || room.type == RoomType.Arena2 || room.type == RoomType.Arena3)
+            //regenerate room shape lists
+            for (int r = 0; r < levelMap.rooms.Count; r++)
             {
-                int enemyAmount;
-                switch (room.type)
+                Room room = levelMap.rooms[r];
+
+                //place enemies
+                if (room.type == RoomType.Arena1 || room.type == RoomType.Arena2 || room.type == RoomType.Arena3)
                 {
-                    case RoomType.Arena1:
-                        enemyAmount = Random.Range(enemyAmountRangeArena1.x, enemyAmountRangeArena1.y);
-                        break;
-                    case RoomType.Arena2:
-                        enemyAmount = Random.Range(enemyAmountRangeArena2.x, enemyAmountRangeArena2.y);
-                        break;
-                    case RoomType.Arena3:
-                        enemyAmount = Random.Range(enemyAmountRangeArena3.x, enemyAmountRangeArena3.y);
-                        break;
-                    default:
-                        enemyAmount = 1;
-                        break;
+                    int enemyAmount;
+                    switch (room.type)
+                    {
+                        case RoomType.Arena1:
+                            enemyAmount = Random.Range(enemyAmountRangeArena1.x, enemyAmountRangeArena1.y);
+                            break;
+                        case RoomType.Arena2:
+                            enemyAmount = Random.Range(enemyAmountRangeArena2.x, enemyAmountRangeArena2.y);
+                            break;
+                        case RoomType.Arena3:
+                            enemyAmount = Random.Range(enemyAmountRangeArena3.x, enemyAmountRangeArena3.y);
+                            break;
+                        default:
+                            enemyAmount = 1;
+                            break;
+                    }
+
+                    for (int i = 0; i < enemyAmount; i++)
+                    {
+                        Vector2Int enemyPositionTile = room.Floor[Random.Range(0, room.Floor.Count - 1)];
+                        Vector3 enemyPosition = new Vector3(enemyPositionTile.x + 0.5f, enemyPositionTile.y + 0.5f, 0);
+                        GameObject go = Instantiate(Random.Range(0, 2) > 0 ? MeleeEnemyPrefab : RangedEnemyPrefab,
+                            enemyPosition, Quaternion.identity);
+                        Enemy e = go.GetComponent<Enemy>();
+                        e.room = r;
+                        EnemyGetCount.enemyCount++;
+                    }
                 }
-                for (int i = 0; i < enemyAmount; i++)
+            }
+        }
+        //assign room id to pre-placed enemies
+        else
+        {
+            //find all game objects tagged "Enemy"
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("Enemy");
+
+            //loop through objects
+            foreach (GameObject enemyObject in objects)
+            {
+                //check if object with enemy tag is an enemy object
+                Enemy enemy = enemyObject.GetComponent<Enemy>();
+                if (enemy != null)
                 {
-                    Vector2Int enemyPositionTile = room.Floor[Random.Range(0, room.Floor.Count - 1)];
-                    Vector3 enemyPosition = new Vector3(enemyPositionTile.x + 0.5f, enemyPositionTile.y + 0.5f, 0);
-                    GameObject go = Instantiate(Random.Range(0, 2) > 0 ? MeleeEnemyPrefab : RangedEnemyPrefab, enemyPosition, Quaternion.identity);
-                    Enemy e = go.GetComponent<Enemy>();
-                    e.room = r;
-                    EnemyGetCount.enemyCount++;
+                    Vector2Int tilePos = new Vector2Int(
+                        Mathf.FloorToInt(transform.position.x),
+                        Mathf.FloorToInt(transform.position.y));
+                    
+                    //check what room the enemy is in
+                    int potentialRoom = levelMap.FindRoom(tilePos);
+                    if (potentialRoom == -1) continue;
+                    
+                    //set room of enemy
+                    enemy.room = potentialRoom;
                 }
             }
         }
