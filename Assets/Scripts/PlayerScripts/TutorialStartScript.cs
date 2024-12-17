ee23a1d846f58aaca0b9452b764c2d148330621c
Nodenyx <albin@alteborn.se>
2024-12-17 18:00:04 +0100

    Added marcus, his anims, a button, and a way to "spawn" player.

--- Assets/Scripts/PlayerScripts/TutorialStartScript.cs
+++ Assets/Scripts/PlayerScripts/TutorialStartScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class TutorialStartScript : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject camera;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerMovement.controlEnabled = false;
+        PlayerAttack.controlEnabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnPlayer()
+    {
+        player.SetActive(true);
+        camera.SetActive(false);
+        PlayerMovement.controlEnabled = true;
+        PlayerAttack.controlEnabled = true;
     }
 }
